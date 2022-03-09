0c76c290cfcb26f44f544f8a87cb9fbf9208f716
Lucas-Masaba <masabaluke@gmail.com>
2022-03-09 23:05:04 +0300

    add reverse tests

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,7 +5,7 @@ describe Solver do
     @solver = Solver.new
   end
 
-  describe 'calling refactor method' do
+  describe 'calling factorial method' do
     it 'should be able to return the factorial of a number' do
       expect(@solver.factorial(5)).to eq(120)
       expect(@solver.factorial(3)).to eq(6)
@@ -16,6 +16,15 @@ describe Solver do
     it 'should be able to raise an exception when a negative value is passed as a parameter' do
       expect {@solver.factorial(-1)}.to raise_error("Can't calculate the factorial of a negative number")
     end
+    
+  end
+
+  describe 'calling reverse method' do
+    it 'should be able to return the reverse of a string' do
+      expect(@solver.reverse('hello')).to eq('olleh')
+      expect(@solver.reverse('eardrummers')).to eq('sremmurdrae')
+    end
+    
   end
 
 end
