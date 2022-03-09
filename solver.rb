d8b912efee46b57038adecb7422a9d6e82c4dbac
ShadyShawkat <shadyshawkat91@gmail.com>
2022-03-09 22:20:46 +0200

    Implement fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -13,4 +13,15 @@ class Solver
     str.reverse
   end
 
+  def fizzbuzz(num)
+    if(num % 3 == 0 && num % 5 == 0)
+      'fizzbuzz'
+    elsif num % 3 ==0
+      'fizz'
+    elsif num % 5 ==0
+      'buzz'
+    else
+      num
+    end
+  end
 end
